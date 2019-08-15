fe7dd1b261d967cc2eddc7e41c80e3d2e9747953
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-08-14 21:09:37 -0600

    Remove internal testing for the most out, left out some as commented out for further investigation

--- src/round.js
+++ src/round.js
@@ -5,6 +5,7 @@ const prototypeQuestions = data.prototypeData;
 const Deck = require('../src/deck');
 const Game = require('../src/Game');
 const Card = require('../src/Card');
+const util = require('./util');
 
 class Round {
   constructor(deck, game) {
@@ -12,9 +13,6 @@ class Round {
     this.turns = 0;
     this.incorrectGuesses = [];
     this.game = game;
-    // ! testing
-    this.round = 0;
-    this.currentRound;
   }
 
   returnCurrentCard() {
@@ -34,33 +32,20 @@ class Round {
     return 100 - (this.incorrectGuesses.length / this.turns) * 100;
   }
 
-  // ! testing
-  start() {
-    this.deck = new Deck(
-      prototypeQuestions.map(cardObject => {
-        return new Card(cardObject);
-      })
-    );
-    this.currentRound = new Round(this.deck);
-    this.printMessage(this.deck, this.currentRound);
-    this.printQuestion(this.currentRound);
-    this.round++;
-  }
-
   endRound() {
-    const percentCorrect = this.calculatePercentCorrect();
+    // const percentCorrect = this.calculatePercentCorrect();
 
     console.log(
       `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly`
     );
 
     // ? does not work right now (want to start new game if answer < 90%)
-    if (percentCorrect < 90) {
-      console.log('test');
-      this.start();
-    } else if (percentCorrect < 100) {
-      console.log('Ok, you are good, but you should try to get 100!');
-    }
+    // if (percentCorrect < 90) {
+    //   console.log('test');
+    //   this.start();
+    // } else if (percentCorrect < 100) {
+    //   console.log('Ok, you are good, but you should try to get 100!');
+    // }
   }
 }
 
