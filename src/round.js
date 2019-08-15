9f7b335826f87769ffe87b077a25af4b4072e67b
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-08-14 21:04:19 -0600

    Testing Round class feature where if < 90% answered correctly, start a new game

--- src/round.js
+++ src/round.js
@@ -1,10 +1,20 @@
 const Turn = require('../src/turn');
+const data = require('./data');
+// ! testing
+const prototypeQuestions = data.prototypeData;
+const Deck = require('../src/deck');
+const Game = require('../src/Game');
+const Card = require('../src/Card');
 
 class Round {
-  constructor(deck) {
+  constructor(deck, game) {
     this.deck = deck || [];
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.game = game;
+    // ! testing
+    this.round = 0;
+    this.currentRound;
   }
 
   returnCurrentCard() {
@@ -13,8 +23,8 @@ class Round {
 
   takeTurn(userGuess) {
     const turn = new Turn(userGuess, this.returnCurrentCard());
-    if (userGuess !== this.deck.deckOfCards[this.turns].correctAnswer) {
-      this.incorrectGuesses.push(this.deck.deckOfCards[this.turns].id);
+    if (userGuess !== this.returnCurrentCard().correctAnswer) {
+      this.incorrectGuesses.push(this.returnCurrentCard().id);
     }
     this.turns++;
     return turn.giveFeedback(userGuess);
@@ -24,8 +34,33 @@ class Round {
     return 100 - (this.incorrectGuesses.length / this.turns) * 100;
   }
 
+  // ! testing
+  start() {
+    this.deck = new Deck(
+      prototypeQuestions.map(cardObject => {
+        return new Card(cardObject);
+      })
+    );
+    this.currentRound = new Round(this.deck);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+    this.round++;
+  }
+
   endRound() {
-    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly`;
+    const percentCorrect = this.calculatePercentCorrect();
+
+    console.log(
+      `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly`
+    );
+
+    // ? does not work right now (want to start new game if answer < 90%)
+    if (percentCorrect < 90) {
+      console.log('test');
+      this.start();
+    } else if (percentCorrect < 100) {
+      console.log('Ok, you are good, but you should try to get 100!');
+    }
   }
 }
 
