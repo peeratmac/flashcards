9150ab89e4ff75336c496b9a91b914b49bea3b25
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-08-14 18:00:19 -0600

    Update gameplay to be able to read number of questions from data.js

--- src/Game.js
+++ src/Game.js
@@ -6,35 +6,24 @@ const Turn = require('../src/turn');
 const Deck = require('../src/deck');
 const Round = require('../src/round');
 
-// TODO:  Your Game class should meet these other requirements:
-// TODO: Should keep track of the currentRound
-// TODO: start: method that starts everything
-// TODO: Creates Cards
-// TODO: Puts Cards in a Deck
-// TODO: Creates a new Round using the Deck
-// TODO: invokes printMessage to display the message in the CLI
-// TODO: invokes printQuestion to kick off our helper functions that allow interaction via the CLI
-
-// ! game.currentRound; // => Round {...} (The new Round object that has been instatiated)
-
 class Game {
   constructor() {
     this.round = 0;
     this.currentRound;
+    this.deck;
   }
 
   start() {
-    const cards = prototypeQuestions.map(object => {
-      const card = new Card({ ...object });
-      return card;
-    });
-    const deck = new Deck(cards);
+    this.deck = new Deck(
+      prototypeQuestions.map(cardObject => {
+        return new Card(cardObject);
+      })
+    );
 
-    const round = new Round(deck);
-    this.currentRound = round;
+    this.currentRound = new Round(this.deck);
 
-    this.printMessage(deck, round);
-    this.printQuestion(round);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
     this.round++;
   }
 
